d7c267bb4892d08c52b072f18737d0f2c8a71f4d
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2019-01-10 16:04:17 +0100

    File reader, not finished

--- src/app/brushes/brushes.component.ts
+++ src/app/brushes/brushes.component.ts
@@ -6,7 +6,20 @@ import { Brush } from '../brush';
   templateUrl: './brushes.component.html',
   styleUrls: ['./brushes.component.css']
 })
-export class BrushesComponent implements OnInit {
+export class BrushesComponent implements OnInit { 
+
+  text: string | ArrayBuffer;
+
+  
+  readFile(): void {
+    let reader=new FileReader();
+    reader.onload = (e) => {
+       this.text=reader.result;
+    }
+    reader.readAsText(new File([""],'../../static/brush-files/bdtest.bt'));   
+  }
+
+  // TODO: Read and parse file. Create brush object
   brushes: Brush[] = [
     { 
       brushId: 1,
@@ -31,6 +44,31 @@ export class BrushesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let text: string; 
+    let reader=new FileReader();
+    reader.onload = (e) => {
+      this.text=reader.result;
+      console.log(reader.result);
+    }
+    reader.readAsText(new File([""],'test.txt')); //'../../static/brush-files/bdtest.bt'
+    console.log(reader.result);
+
   }
 
 }
+
+/*
+function read() {
+  //let brushes: Brush[]
+  let result: string;
+
+  let reader = new FileReader();
+  reader.onload = (e) => {
+    this.result = reader.result;
+  }
+
+  this.result = reader.readAsText(new File([""],'../../static/brush-files/bdtest.bt'));
+  console.log(this.result);
+  
+  //return result
+}*/
