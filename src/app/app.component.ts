84254590e2378b31792eae93102517e2c252b01b
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2019-01-10 14:24:14 +0100

    Dummy data and brushfiles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Brush editor';
+  brush = {
+    brushId: 1,
+    ch1: 10,
+    ch2: 0,
+    ch3: 200,
+    ch4: 200, 
+    ch5: 100,
+    desc: "Støtfanger"
+  }
+  brush2 = {
+    brushId: 2,
+    ch1: 50,
+    ch2: 20,
+    ch3: 100,
+    ch4: 500,
+    ch5: 75,
+    desc: "Tak"
+  }
+  allBrushes = [this.brush, this.brush2]
 }
---
+++ src/app/brush.ts
@@ -0,0 +1,16 @@
+export class Brush {
+    brushId: number;
+    ch1: number;
+    ch2: number;
+    ch3: number;
+    ch4: number;
+    desc: string
+
+    
+    //brushId: 2; 
+    //ch1: 0; 
+    //ch2: 0; 
+    //ch3: 0; 
+    //ch4: 0; 
+    //desc: "Heihei"
+}
