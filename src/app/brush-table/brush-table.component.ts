0432b2a0a695debb101000db6cdbd912dcf486dc
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2019-01-11 18:01:34 +0100

    Can read files with different amount of channels

--- src/app/brush-table/brush-table.component.ts
+++ src/app/brush-table/brush-table.component.ts
@@ -35,26 +35,29 @@ export class BrushTableComponent implements OnInit {
     let list: string[] = text.split(/\r?\n/);
     let counter: number = 1;
     
-    // Loop trough brushes from file and push to brush-object
+    // Loop through brushes from file and push to brush-object
     list.forEach(element => {
       let channels: string[] = element.split(",");
       
       // Handle potential comments in brush file
-      let description: string = channels[4].split("#")[1];
-      channels[4] = channels[4].split("#")[0];
+      let lastCh = channels.length-1;
+      let description: string = channels[lastCh].split("#")[1];
+      channels[lastCh] = channels[lastCh].split("#")[0];
       if (description === undefined) {
         description = "";
       }
 
+      // Add the correct amount of channels from file
       this.brushes.push({
-        brushId: counter,
-        ch1: +channels[0],
-        ch2: +channels[1],
-        ch3: +channels[2],
-        ch4: +channels[3],
-        ch5: +channels[4],
-        desc: description
-      });
+          brushId: counter,
+          ch1: +channels[0],
+          ch2: +channels[1],
+          ch3: +channels[2],
+          ch4: +channels[3],
+          ch5: +channels[4],
+          desc: description
+        });
+      
       counter++;
     });
   }
