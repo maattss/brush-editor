c814c164eca0e3b671218135f812022752c9c378
Mats Tyldum <ma.tyldum@gmail.com>
2019-01-18 15:40:56 +0100

    Can now edit channel values and graph will update!
    + other minor fixes and styling updates

--- src/app/brush-table/brush-table.component.ts
+++ src/app/brush-table/brush-table.component.ts
@@ -8,17 +8,18 @@ import { BrushService } from '../brush.service';
   styleUrls: ['./brush-table.component.scss']
 })
 export class BrushTableComponent implements OnInit {
-  brushes: Brush[];
   constructor(private data: BrushService) { }
-  currentBrushId: number = 1;
 
   ngOnInit() {
     this.data.currentBrush.subscribe(brushes => this.brushes = brushes);
   }
 
+  // Local variables
+  brushes: Brush[];
+  currentBrushId: number = 1;
   file: any;
 
-  // Chart
+  // Chart variables
   public barChartOptions:any = {
     scaleShowVerticalLines: false,
     responsive: true,
@@ -32,10 +33,14 @@ export class BrushTableComponent implements OnInit {
       ]
     }
   };
-  public barChartLabels:string[] = [];
-  public barChartData:any[] = [];
+  public barChartLabels:string[] = ["No data"];
+  public barChartData:any[] = [{
+    data: 0,
+    label: "No data"
+  }];
   public barChartType:string = 'bar';
   public barChartLegend:boolean = true;
+  public isDataAvailable:boolean = false; // Controls if graph is displayed or not
 
   // Checks the values of the input fields. Allows numbers and "."
   numberOnly(event: any): boolean {
@@ -46,7 +51,7 @@ export class BrushTableComponent implements OnInit {
     return true;
   }
 
-  // Add labels to graph
+  // Add/update labels to graph
   addLabels() {
     let totalChannels = this.returnAmountOfChannels();
     let channelString = "";
@@ -58,18 +63,16 @@ export class BrushTableComponent implements OnInit {
     }
   }
 
-  // Add data to graph
+  // Add/update data to graph
   addData(brushID: number) {
     this.addLabels();
+    
+    this.isDataAvailable = true;
     this.barChartData = [];
     let br: Brush = this.brushes[brushID - 1];
     let values: number[] = [br.ch1, br.ch2, br.ch3]
-    if (br.ch4 >= 0) {
-      values.push(br.ch4);
-    }
-    if (br.ch5 >= 0) {
-      values.push(br.ch5);
-    }
+    if (br.ch4 >= 0) { values.push(br.ch4) };
+    if (br.ch5 >= 0) { values.push(br.ch5) };
 
     this.barChartData.push({
       data: values,
@@ -81,44 +84,60 @@ export class BrushTableComponent implements OnInit {
     clone[0].data = values;
     clone[0].label = "BrushID " + brushID;
     this.barChartData = clone;
-    
   }
 
   returnAmountOfChannels() {
     let totalChannels = 0;
-    if(this.brushes[0].ch3>=0) { 
-      totalChannels = 3;
-    }
-    if(this.brushes[0].ch4>=0) {
-      totalChannels = 4;
-    }
-    if(this.brushes[0].ch5>=0) {
-      totalChannels = 5;
-    }
+    if(this.brushes[0].ch3>=0) { totalChannels = 3 };
+    if(this.brushes[0].ch4>=0) { totalChannels = 4 };
+    if(this.brushes[0].ch5>=0) { totalChannels = 5 };
+    
     return totalChannels;
   }
 
   markRow(rowId: number) {
     this.currentBrushId = rowId;
-    let totalChannels = 0;
+    var totalChannels = 0;
+    var colorClass = "table-success";
     totalChannels = this.returnAmountOfChannels();
 
     // Clear color of all inactive rows
     for(var rowIndex=1; rowIndex<=this.brushes.length; rowIndex++) {
       for(var chIndex=1; chIndex<=totalChannels; chIndex++) {
         let chName = "ch" + chIndex;
-        document.getElementById(rowIndex + chName).classList.remove("bg-success");
+        document.getElementById(rowIndex + chName).classList.remove(colorClass);
       }
-      document.getElementById(rowIndex.toString()).classList.remove("bg-success");
-      document.getElementById(rowIndex + "desc").classList.remove("bg-success");
+      document.getElementById(rowIndex.toString()).classList.remove(colorClass);
+      document.getElementById(rowIndex + "desc").classList.remove(colorClass);
     }
 
-    // Setting color of the active row
+    // Set color of the active row
     for(var chIndex=1; chIndex<=totalChannels; chIndex++) {
       let chName = "ch" + chIndex;
-      document.getElementById(rowId + chName).classList.add("bg-success");
+      document.getElementById(rowId + chName).classList.add(colorClass);
     }
-    document.getElementById(rowId.toString()).classList.add("bg-success");
-    document.getElementById(rowId + "desc").classList.add("bg-success");
+    document.getElementById(rowId.toString()).classList.add(colorClass);
+    document.getElementById(rowId + "desc").classList.add(colorClass);
+  }
+
+  updateBrushes(brushId: number, channel: string) {
+    let elementId = "" + brushId + channel;
+    let val = (<HTMLInputElement>document.getElementById(elementId)).value
+
+    var brush = this.brushes[brushId-1];
+    if (channel === "ch1") {
+      brush.ch1 = +val; // + parses string to number
+    } else if (channel === "ch2") {
+      brush.ch2 = +val;
+    } else if (channel === "ch3") {
+      brush.ch3 = +val;
+    } else if (channel === "ch4") {
+      brush.ch4 = +val;
+    } else if (channel === "ch5") {
+      brush.ch5 = +val;
+    } else {  // channel === "desc"
+      brush.desc = val;
+    }
+    this.addData(brushId);  // Update graph
   }
 }
