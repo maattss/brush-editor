656fc614bfbfc5e5841915dcb929b8a0599acdbe
Mats Tyldum <ma.tyldum@gmail.com>
2019-02-14 13:44:26 +0100

    Second navbar update
    Not finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Brush } from './brush';
+import { BrushService } from './_services/index';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+  constructor(private data: BrushService) { }
 
+  // Class variables
+  private brushes: Brush[];
+
+  ngOnInit() {
+    // Subscribe
+    this.data.currentBrush.subscribe(brushes => this.brushes = brushes);
+  }
 }
