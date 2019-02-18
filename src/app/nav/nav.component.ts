7dad64fc4489cab0fc64f77954780bcdb3c89b56
Mats Tyldum <ma.tyldum@gmail.com>
2019-02-18 09:42:43 +0100

    Navbar update

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -5,7 +5,7 @@ import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-nav',
-  templateUrl: './nav.component2.html',
+  templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
