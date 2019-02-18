f9c9139e3fe6a63338aa232c3923ffc8d90572c6
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2019-02-18 11:04:10 +0100

    New modules added for form and tooltips

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Modules
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ChartsModule } from 'ng2-charts';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
 import { AppComponent } from './app.component';
 import { BrushTableComponent } from './brush-table/brush-table.component';
 import { NavComponent } from './nav/nav.component';
 import { BrushSettingsComponent } from './brush-settings/brush-settings.component';
 import { BrushGraphComponent } from './brush-graph/brush-graph.component';
-import { ChartsModule } from 'ng2-charts';
-import { CookieService } from 'ngx-cookie-service';
-
-import { PagerService } from './_services/index';
 import { BrushInfoComponent } from './brush-info/brush-info.component';
+
+// Services
+import { CookieService } from 'ngx-cookie-service';
+import { PagerService } from './_services/index';
 
 @NgModule({
   declarations: [
@@ -25,7 +31,11 @@ import { BrushInfoComponent } from './brush-info/brush-info.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ChartsModule
+    ChartsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTooltipModule,
+    BrowserAnimationsModule
   ],
   providers: [
     CookieService,
