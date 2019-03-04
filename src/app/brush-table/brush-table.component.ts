cccda6c6e3c2af8852db525e6967b13b9fdfb502
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2019-03-04 22:15:35 +0100

    Found a better way of looping through brushes and made some user improvements, bug fix, etc..

--- src/app/brush-table/brush-table.component.ts
+++ src/app/brush-table/brush-table.component.ts
@@ -61,32 +61,16 @@ export class BrushTableComponent implements OnInit {
     }
   }
 
-  private inputValidation(brushId: number): void {
-    console.log('Checkinggggggg');
+  private inputValidation(brushId: number, channel): void {
     this.view.showInfoError('Maximum input value is ' + this.maxChannelValue);
-    if (this.brushes[brushId - 1].ch1 > this.maxChannelValue) {
-      while (this.brushes[brushId - 1].ch1 > this.maxChannelValue) { // In case user holds button
-        this.brushes[brushId - 1].ch1 = Math.floor(this.brushes[brushId - 1].ch1 / 10);
+    const brush = this.brushes[brushId - 1];
+    for (const channelX in brush) { // Loops through channel names in brush
+      if (channelX.toString() === channel && brush[channelX] > this.maxChannelValue) {
+        brush[channelX] = Math.floor(brush[channelX] / 10);
+        return;
       }
-    } else if (this.brushes[brushId - 1].ch2 > this.maxChannelValue) {
-      while (this.brushes[brushId - 1].ch2 > this.maxChannelValue) {
-        this.brushes[brushId - 1].ch2 = Math.floor(this.brushes[brushId - 1].ch2 / 10);
-      }
-    } else if (this.brushes[brushId - 1].ch3 > this.maxChannelValue) {
-      while (this.brushes[brushId - 1].ch3 > this.maxChannelValue) {
-        this.brushes[brushId - 1].ch3 = Math.floor(this.brushes[brushId - 1].ch3 / 10);
-      }
-    } else if (this.brushes[brushId - 1].ch4 > this.maxChannelValue) {
-      while (this.brushes[brushId - 1].ch4 > this.maxChannelValue) {
-        this.brushes[brushId - 1].ch4 = Math.floor(this.brushes[brushId - 1].ch4 / 10);
-      }
-    } else if (this.brushes[brushId - 1].ch5 > this.maxChannelValue) {
-      while (this.brushes[brushId - 1].ch5 > this.maxChannelValue) {
-        this.brushes[brushId - 1].ch5 = Math.floor(this.brushes[brushId - 1].ch5 / 10);
-      }
-    } else {
-      this.view.closeInfoError();
     }
+    this.view.closeInfoError();
     this.data.changeBrush(this.brushes);
   }
 
@@ -117,18 +101,12 @@ export class BrushTableComponent implements OnInit {
   }
 
   changeChannelValue(channelId: number) {
-    const channelName = (<HTMLInputElement>document.getElementById('channelName' + channelId)).value;
+    const newChannelName = (<HTMLInputElement>document.getElementById('channelName' + channelId)).value;
 
-    if (channelId === 1) {
-      this.channelNames.ch1 = channelName;
-    } else if (channelId === 2) {
-      this.channelNames.ch2 = channelName;
-    } else if (channelId === 3) {
-      this.channelNames.ch3 = channelName;
-    } else if (channelId === 4) {
-      this.channelNames.ch4 = channelName;
-    } else if (channelId === 5) {
-      this.channelNames.ch5 = channelName;
+    for (const oldName in this.channelNames) {
+      if (oldName === ('ch' + channelId.toString())) {
+        this.channelNames[oldName] = newChannelName;
+      }
     }
     this.data.changeChannelName(this.channelNames);
   }
@@ -142,31 +120,28 @@ export class BrushTableComponent implements OnInit {
   updateBrushes(brushId: number, channel: string) {
     const elementId = '' + brushId + channel;
     const inputValue = (<HTMLInputElement>document.getElementById(elementId)).value;
-
     const brush = this.brushes[brushId - 1];
-    if (channel === 'ch1') {
-      brush.ch1 = +inputValue; // + parses string to number
-    } else if (channel === 'ch2') {
-      brush.ch2 = +inputValue;
-    } else if (channel === 'ch3') {
-      brush.ch3 = +inputValue;
-    } else if (channel === 'ch4') {
-      brush.ch4 = +inputValue;
-    } else if (channel === 'ch5') {
-      brush.ch5 = +inputValue;
-    } else {  // channel === "desc"
-      brush.desc = inputValue;
+
+    for (const channelX in brush) { // Loops through channel names in brush
+      if (channelX.toString() === channel) {
+        if (channel === 'desc') {
+          brush[channelX] = inputValue;
+        } else { // ch1, ch2, ch3, ch4 or ch5
+          brush[channelX] = +inputValue;
+        }
+      }
     }
   }
 
   deleteRow(brushId: number) {
     const brush = this.brushes[brushId - 1];
-    brush.ch1 = 0;
-    brush.ch2 = 0;
-    brush.ch3 = 0;
-    brush.ch4 = 0;
-    brush.ch5 = 0;
-    brush.desc = '';
+    for (const channelX in brush) { // Loops through channel names in brush
+      if (channelX.toString() === 'desc') {
+        brush[channelX] = '';
+      } else if (channelX.toString() !== 'brushId') {
+        brush[channelX] = 0;
+      }
+    }
     this.data.changeBrush(this.brushes);
   }
 }
