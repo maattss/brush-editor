89c9ed74826495d800dbc84ada443cdcb65afb1e
Mats Tyldum <ma.tyldum@gmail.com>
2019-03-05 15:33:19 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,13 @@ import { BrushService, ViewService } from './_services/index';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  implements OnInit {
-  constructor(private data: BrushService, private view: ViewService) { }
+  constructor(private data: BrushService) { }
 
   // Class variables
   private brushes: Brush[];
-  private showFileInfo: boolean;
-  private showSettings: boolean;
-  private showFileChooser: boolean;
-  private showInfoMsg: boolean;
 
   ngOnInit() {
     // Subscribe
     this.data.currentBrush.subscribe(brushes => this.brushes = brushes);
-    // this.view.infoMsg.subscribe(showInfoMsg => this.InfoMsg = showInfoMsg);
   }
 }
