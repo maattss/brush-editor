929a28d4d25e926a84916df86937cdd21e893372
Mats Tyldum <ma.tyldum@gmail.com>
2019-05-01 15:52:57 +0200

    Update pager.service.ts

--- src/app/_services/pager.service.ts
+++ src/app/_services/pager.service.ts
@@ -9,10 +9,10 @@ export class PagerService {
         currentPage: number = 1,
         pageSize: number = 10
     ) {
-        // calculate total pages
+        // Calculate total pages
         const totalPages = Math.ceil(totalItems / pageSize);
 
-        // ensure current page isn't out of range
+        // Ensure current page isn't out of range
         if (currentPage < 1) {
             currentPage = 1;
         } else if (currentPage > totalPages) {
@@ -21,11 +21,11 @@ export class PagerService {
 
         let startPage: number, endPage: number;
         if (totalPages <= 10) {
-            // less than 10 total pages so show all
+            // Less than 10 total pages so show all
             startPage = 1;
             endPage = totalPages;
         } else {
-            // more than 10 total pages so calculate start and end pages
+            // More than 10 total pages so calculate start and end pages
             if (currentPage <= 6) {
                 startPage = 1;
                 endPage = 10;
@@ -38,17 +38,17 @@ export class PagerService {
             }
         }
 
-        // calculate start and end item indexes
+        // Calculate start and end item indexes
         const startIndex = (currentPage - 1) * pageSize;
         const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
-        // create an array of pages to ng-repeat in the pager control
+        // Create an array of pages to ng-repeat in the pager control
         // tslint:disable-next-line:prefer-const
         let pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
             i => startPage + i
         );
 
-        // return object with all pager properties required by the view
+        // Return object with all pager properties required by the view
         return {
             totalItems: totalItems,
             currentPage: currentPage,
